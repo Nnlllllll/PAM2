d8b7618b8df7e2b1878d4f13d497badfb691dfb1
Nnlllllll <nathannovais020@gmail.com>
2026-03-31 09:56:25 -0300

    Add files via upload

---
+++ PAM2/meuCrudApp/src/servers/configApi.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://good-memes-scream.loca.lt"
---
+++ PAM2/meuCrudApp/src/servers/peopleCrud.js
@@ -0,0 +1,44 @@
+import { API_URL } from "./configApi";
+
+export async function getPeople() {
+  const response = await fetch(`${API_URL}/people`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Bypass-Tunnel-Reminder": "true"
+    }});
+  const data = await response.json();
+  return data;
+}
+
+export async function createPerson(person) {
+  const response = await fetch(`${API_URL}/people`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Bypass-Tunnel-Reminder": "true"
+    },
+    body: JSON.stringify(person)
+  });
+
+  return response.json();
+}
+
+export async function updatePerson(id, person) {
+    const response = await fetch(`${API_URL}/people/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Bypass-Tunnel-Reminder": "true"
+      },
+      body: JSON.stringify(person)
+    });
+  
+    return response.json();
+  }
+  
+  export async function deletePerson(id) {
+    await fetch(`${API_URL}/people/${id}`, {
+      method: "DELETE"
+    });
+  }
